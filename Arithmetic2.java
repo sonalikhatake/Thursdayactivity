c91aa3b0ae73c294ee9fcc6a71d4e561a41c3d2b
sonalikhatake <118472377+sonalikhatake@users.noreply.github.com>
2022-11-25 11:25:17 +0530

    Add files via upload

---
+++ Arithmetic2.java
@@ -0,0 +1,16 @@
+//author:Dwarka
+// code to display sum of 2 no
+
+package com.question2;
+class Arithmetic{
+    public int add(int a, int b){
+        int sum = a + b; 
+        return sum;
+    }
+}
+
+class Adder extends Arithmetic{  
+    public int callAdd(int a, int b){
+        return add(a, b);   //add
+    }
+}
