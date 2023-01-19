055e7d2d10dd59e6d34061a70938005b3341850a
sonalikhatake <118472377+sonalikhatake@users.noreply.github.com>
2023-01-19 23:55:26 +0530

    Add files via upload

---
+++ Pyramid .java
@@ -0,0 +1,20 @@
+//program that prints a pyramid of stars(*)
+package com.question4;
+
+public class Pyramid {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;++i) {
+            for(int j=1;j<=5-i;++j) 
+            {
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; ++j) 
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+		}
+	}
+
+}
---
+++ SearchArray .java
@@ -0,0 +1,30 @@
+//program to search an element in an array
+package com.question6;
+
+public class SearchArray {
+
+	public static void main(String[] args) {
+		int[] myArray={1, 2, 3, 4, 5};
+        int searchValue=3;
+        boolean found=false;
+
+        for (int i=0;i<myArray.length;i++) 
+        {
+            if (myArray[i]==searchValue) 
+            {
+                found=true;
+                break;
+            }
+        }
+
+        if (found) 
+        {
+            System.out.println(searchValue+" was found in the array");
+        } 
+        else 
+        {
+            System.out.println(searchValue+" was not found in the array");
+        }
+	}
+
+}
